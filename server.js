bb77b76c0755a88ecedde457029f38a748b5ad88
JAYANTH KUMAR <120399385+JOY-BOY-7@users.noreply.github.com>
2025-09-29 12:16:00 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set up a route to serve the main HTML file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Custom Element Service listening at http://localhost:${port}`);
+});
